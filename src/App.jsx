17e07e41f5a5cc664ad853853d927cdd59a25727
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-01-21 16:34:39 -0300

    formulario-react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Louro from './Components/Louro'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+       <Louro />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Louro.jsx
@@ -0,0 +1,16 @@
+function Louro() {
+    return(
+        <div className="oee">
+            <form action="">
+                <h1>Preencha o formulario</h1>
+                <p>Nome: <input type="text" name="" id="" /></p>
+                <p>Email: <input type="email" name="" id="" /></p>
+                <p>Senha: <input type="password" name="" id="" /></p>
+                <p><input  className="button" type="button" value="ENTRAR" /></p>
+                <p>Esqueceu a senha? <a href="#">Recuperar</a></p>
+            </form>
+        </div>
+    )
+}
+
+export default Louro
