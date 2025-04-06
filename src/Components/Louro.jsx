b8cc5c1364b344c8c7db8aaf48e526965b6896db
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-04-05 22:51:59 -0300

    Adicionando uma classe no meu paragrafo recuperar senha, e colocando o paragrafo no fim do formulario.

--- src/Components/Louro.jsx
+++ src/Components/Louro.jsx
@@ -7,7 +7,7 @@ function Louro() {
                 <p>Email: <input type="email" name="" id="" /></p>
                 <p>Senha: <input type="password" name="" id="" /></p>
                 <p><input  className="button" type="button" value="ENTRAR" /></p>
-                <p>Esqueceu a senha? <a href="#">Recuperar</a></p>
+                <p className="recuperar">Esqueceu a senha? <a href="#">Recuperar</a></p>
             </form>
         </div>
     )
